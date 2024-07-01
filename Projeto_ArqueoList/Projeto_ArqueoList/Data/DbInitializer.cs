337c1980be7711c4143797b73a7a56850394efaf
TiagoDias2 <tiagomiguel082004@gmail.com>
2024-07-01 19:54:57 +0100

    DbInitializer

---
+++ Projeto_ArqueoList/Projeto_ArqueoList/Data/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Aulas.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Aulas.Data
+{
+
+    internal class DbInitializer
+    {
+
+        internal static async void Initialize(ApplicationDbContext dbContext)
+        {
+
+            ArgumentNullException.ThrowIfNull()
+        }
+    }
+}
